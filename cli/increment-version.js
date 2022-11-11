55c1c070589183cae6000824df71aa8a9c2dac5c
zyrouge <naveencnaveen96@gmail.com>
2022-11-11 17:50:34 +0530

    chore: initial version

--- cli/increment-version.js
+++ cli/increment-version.js
@@ -1,18 +1,12 @@
 const versioner = require("./helpers/version");
 
 const main = async () => {
-    const args = process.argv.slice(2);
-    if (!args.includes("-y")) throw new Error("Missing flag '-y'");
     const currentVersion = await versioner.getVersion();
     const versionCode = currentVersion.versionCode + 1;
     const date = new Date();
-    const versionName = [
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate(),
-        versionCode
-    ].map(x => x.toString()).join(".");
-    return versioner.updateVersion({ versionCode, versionName });
+    const versionName = `${date.getFullYear()}.${date.getMonth()}.${versionCode}`;
+    await versioner.updateVersion({ versionCode, versionName });
+    console.log(`Version bumped from ${currentVersion.versionName} to ${versionName}!`);
 }
 
 main();
