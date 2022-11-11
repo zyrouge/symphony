ac0159b6b45df655075ba1da7a175857386f84f6
zyrouge <naveencnaveen96@gmail.com>
2022-11-11 18:57:47 +0530

    chore: lint

--- cli/increment-version.js
+++ cli/increment-version.js
@@ -4,7 +4,7 @@ const main = async () => {
     const currentVersion = await versioner.getVersion();
     const versionCode = currentVersion.versionCode + 1;
     const date = new Date();
-    const versionName = `${date.getFullYear()}.${date.getMonth()}.${versionCode}`;
+    const versionName = `${date.getFullYear()}.${date.getMonth() + 1}.${versionCode}`;
     await versioner.updateVersion({ versionCode, versionName });
     console.log(`Version bumped from ${currentVersion.versionName} to ${versionName}!`);
 }
