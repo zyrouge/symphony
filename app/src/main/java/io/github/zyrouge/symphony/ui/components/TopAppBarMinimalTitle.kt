5986173ba6154b76e9f6adae0932a911838e0e97
zyrouge <naveencnaveen96@gmail.com>
2022-11-19 14:11:04 +0530

    fix: appbar text overlap

--- app/src/main/java/io/github/zyrouge/symphony/ui/components/TopAppBarMinimalTitle.kt
+++ app/src/main/java/io/github/zyrouge/symphony/ui/components/TopAppBarMinimalTitle.kt
@@ -1,19 +1,34 @@
 package io.github.zyrouge.symphony.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun TopAppBarMinimalTitle(content: @Composable () -> Unit) {
-    ProvideTextStyle(
-        MaterialTheme.typography.bodyLarge.copy(
-            fontWeight = FontWeight.Bold,
-            letterSpacing = 0.sp
-        )
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        contentAlignment = Alignment.Center,
     ) {
-        content()
+        ProvideTextStyle(
+            MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold,
+                letterSpacing = 0.sp,
+                textAlign = TextAlign.Center,
+            )
+        ) {
+            content()
+        }
     }
 }
