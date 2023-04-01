6735fe49d76c2f7917b28e2d38e3eb79891d5a53
Zyrouge <zyrouge@hotmail.com>
2023-04-01 21:28:28 +0530

    refactor: use #108

--- app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translations.kt
+++ app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translations.kt
@@ -1,10 +1,17 @@
 package io.github.zyrouge.symphony.services.i18n
 
+import io.github.zyrouge.symphony.services.i18n.translations.BeTranslation
 import io.github.zyrouge.symphony.services.i18n.translations.EnTranslation
-import io.github.zyrouge.symphony.services.i18n.translations.ITranslations
+import io.github.zyrouge.symphony.services.i18n.translations.RuTranslation
+import io.github.zyrouge.symphony.services.i18n.translations.UkTranslation
 
 object Translations {
-    val all = arrayOf<ITranslations>(EnTranslation())
+    val all = arrayOf(
+        EnTranslation(),
+        BeTranslation(),
+        RuTranslation(),
+        UkTranslation(),
+    )
     val default = all.first()
 
     fun fromLocale(locale: String) = all.find {
--- app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translator.kt
+++ app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translator.kt
@@ -1,14 +1,24 @@
 package io.github.zyrouge.symphony.services.i18n
 
 import io.github.zyrouge.symphony.Symphony
+import io.github.zyrouge.symphony.services.SettingsKeys
 import io.github.zyrouge.symphony.services.i18n.translations.ITranslations
 
 class Translator(private val symphony: Symphony) {
     var t: ITranslations
 
     init {
-        t = symphony.settings.getLanguage()
-            ?.let { Translations.fromLocale(it) }
-            ?: Translations.default
+        t = getCurrentTranslations()
+        symphony.settings.onChange.subscribe { key ->
+            when (key) {
+                SettingsKeys.language -> {
+                    t = getCurrentTranslations()
+                }
+            }
+        }
     }
+
+    fun getCurrentTranslations() = symphony.settings.getLanguage()
+        ?.let { Translations.fromLocale(it) }
+        ?: Translations.default
 }
