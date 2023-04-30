b33fd39ceaa5046167323d5aefa18945890d61a1
Zyrouge <zyrouge@hotmail.com>
2023-04-30 13:52:19 +0530

    refactor: update i18n

--- app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translations.kt
+++ app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translations.kt
@@ -1,15 +1,14 @@
 package io.github.zyrouge.symphony.services.i18n
 
-import io.github.zyrouge.symphony.services.i18n.translations.BeTranslation
-import io.github.zyrouge.symphony.services.i18n.translations.EnTranslation
-import io.github.zyrouge.symphony.services.i18n.translations.RuTranslation
-import io.github.zyrouge.symphony.services.i18n.translations.UkTranslation
+import io.github.zyrouge.symphony.services.i18n.translations.*
 
 object Translations {
     val all = arrayOf(
         EnTranslation(),
         BeTranslation(),
+        ItTranslation(),
         RuTranslation(),
+        TrTranslation(),
         UkTranslation(),
     )
     val default = all.first()
