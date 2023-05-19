0e2733716342124e01dacce9dd6458060c6497ac
Zyrouge <zyrouge@hotmail.com>
2023-05-19 17:19:07 +0530

    refactor: remove launched effect from eventer effect

--- app/src/main/java/io/github/zyrouge/symphony/ui/components/EventerEffect.kt
+++ app/src/main/java/io/github/zyrouge/symphony/ui/components/EventerEffect.kt
@@ -2,23 +2,16 @@ package io.github.zyrouge.symphony.ui.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import io.github.zyrouge.symphony.utils.EventUnsubscribeFn
 import io.github.zyrouge.symphony.utils.Eventer
 
 @Composable
 fun <T> EventerEffect(eventer: Eventer<T>, onEvent: (T) -> Unit) {
-    var unsubscribe: EventUnsubscribeFn? = remember { null }
-
-    LaunchedEffect(LocalLifecycleOwner.current) {
-        unsubscribe = eventer.subscribe {
+    DisposableEffect(LocalLifecycleOwner.current) {
+        val unsubscribe = eventer.subscribe {
             onEvent(it)
         }
-    }
 
-    DisposableEffect(LocalLifecycleOwner.current) {
-        onDispose { unsubscribe?.invoke() }
+        onDispose { unsubscribe.invoke() }
     }
 }
