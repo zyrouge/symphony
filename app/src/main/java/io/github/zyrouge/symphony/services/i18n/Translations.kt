3595a1161fb800018d667a5b17a9bd10367885a4
Icarus Xu <o0icarusxu0o@gmail.com>
2023-06-23 15:02:35 +0800

    add country code for simplified chinese
    Signed-off-by: Icarus Xu <o0icarusxu0o@gmail.com>

--- app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translations.kt
+++ app/src/main/java/io/github/zyrouge/symphony/services/i18n/Translations.kt
@@ -8,7 +8,7 @@ import io.github.zyrouge.symphony.services.i18n.translations.ItTranslation
 import io.github.zyrouge.symphony.services.i18n.translations.RuTranslation
 import io.github.zyrouge.symphony.services.i18n.translations.TrTranslation
 import io.github.zyrouge.symphony.services.i18n.translations.UkTranslation
-import io.github.zyrouge.symphony.services.i18n.translations.ZhTranslation
+import io.github.zyrouge.symphony.services.i18n.translations.ZhCnTranslation
 
 object Translations {
     private val enTranslation = EnTranslation()
@@ -20,7 +20,7 @@ object Translations {
         RuTranslation(),
         TrTranslation(),
         UkTranslation(),
-        ZhTranslation(),
+        ZhCnTranslation(),
     )
 
     fun get(locale: String) = all[locale]
