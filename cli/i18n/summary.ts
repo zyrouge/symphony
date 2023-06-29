088fa07ea1dd1b16c5db994702c29a4a7c00adb4
zyrouge <zyrouge@hotmail.com>
2023-06-29 17:59:30 +0530

    chore: fix translation percent

--- cli/i18n/summary.ts
+++ cli/i18n/summary.ts
@@ -31,8 +31,7 @@ ${Object.entries(summary.individual)
         `.trim()
     );
     const translationPercent = Math.floor(
-        ((summary.total.set + summary.total.defaulted) / summary.total.total) *
-            100
+        (summary.total.set / summary.total.total) * 100
     );
     await fs.writeFile(
         path.join(outputDir, `badge-translated.json`),
