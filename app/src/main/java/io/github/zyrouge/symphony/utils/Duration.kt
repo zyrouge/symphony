0f49a4020d5df4685cd446e2fb2ba7b68f714fd9
zyrouge <zyrouge@hotmail.com>
2023-07-13 20:47:56 +0530

    fix: support displaying days and hours (#200)

--- app/src/main/java/io/github/zyrouge/symphony/utils/Duration.kt
+++ app/src/main/java/io/github/zyrouge/symphony/utils/Duration.kt
@@ -4,12 +4,16 @@ import java.util.concurrent.TimeUnit
 
 object DurationFormatter {
     fun formatMs(ms: Int) = formatMs(ms.toLong())
-    fun formatMs(ms: Long): String {
-        return formatMinSec(
-            TimeUnit.MILLISECONDS.toMinutes(ms) % TimeUnit.HOURS.toMinutes(1),
-            TimeUnit.MILLISECONDS.toSeconds(ms) % TimeUnit.MINUTES.toSeconds(1)
-        )
-    }
+    fun formatMs(ms: Long) = formatMinSec(
+        TimeUnit.MILLISECONDS.toDays(ms).floorDiv(TimeUnit.DAYS.toDays(1)),
+        TimeUnit.MILLISECONDS.toHours(ms) % TimeUnit.DAYS.toHours(1),
+        TimeUnit.MILLISECONDS.toMinutes(ms) % TimeUnit.HOURS.toMinutes(1),
+        TimeUnit.MILLISECONDS.toSeconds(ms) % TimeUnit.MINUTES.toSeconds(1)
+    )
 
-    fun formatMinSec(m: Long, s: Long) = String.format("%02d:%02d", m, s)
+    fun formatMinSec(d: Long, h: Long, m: Long, s: Long) = when {
+        d == 0L && h == 0L -> String.format("%02d:%02d", m, s)
+        d == 0L -> String.format("%02d:%02d:%02d", h, m, s)
+        else -> String.format("%02d:%02d:%02d:%02d", d, h, m, s)
+    }
 }
