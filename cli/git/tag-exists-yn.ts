cb45fe74d16a1419527b011ad20dce3456cd5d8f
zyrouge <zyrouge@hotmail.com>
2023-10-24 22:27:25 +0530

    ci: better nightly release check

--- cli/git/tag-exists-yn.ts
+++ cli/git/tag-exists-yn.ts
@@ -4,8 +4,7 @@ const main = async () => {
     const [tag] = process.argv.slice(2);
     if (!tag) throw new Error("Missing argument: tag");
     const exists = await Git.tagExists(tag);
-    if (!exists) throw new Error(`Tag ${tag} already exists`);
-    console.log(`Tag ${tag} is available`);
+    console.log(exists ? "yes" : "no");
 };
 
 main();
---
+++ cli/helpers/git.ts
@@ -0,0 +1,49 @@
+import { spawnSync } from "child_process";
+
+export interface GitSpawnResult {
+    status: number | null;
+    stdout: string;
+    stderr: string;
+}
+
+export class Git {
+    static async getLatestRevisionShort() {
+        const proc = await Git.spawn(["rev-parse", "--short", "HEAD"]);
+        if (proc.status !== 0) throw new Error(`Unable to get revision`);
+        return proc.stdout;
+    }
+
+    static async getRevisionDate(sha: string) {
+        const proc = await Git.spawn([
+            "show",
+            "--no-patch",
+            "--no-notes",
+            "--pretty='%cI'",
+            sha,
+        ]);
+        if (proc.status !== 0) throw new Error(`Unable to get revision date`);
+        const isoString = proc.stdout.slice(1, -1);
+        return new Date(isoString);
+    }
+
+    static async tagExists(tag: string) {
+        const proc = await Git.spawn([
+            "ls-remote",
+            "--exit-code",
+            "--tags",
+            "origin",
+            tag,
+        ]);
+        return proc.status !== 0;
+    }
+
+    static async spawn(args: string[]) {
+        const proc = spawnSync("git", args);
+        const result: GitSpawnResult = {
+            status: proc.status,
+            stdout: proc.stdout.toString().trim(),
+            stderr: proc.stderr.toString().trim(),
+        };
+        return result;
+    }
+}
