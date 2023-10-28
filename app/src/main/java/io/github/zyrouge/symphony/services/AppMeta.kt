ee07707edbeb04a6f4b95947d5d8defeefe8e8a2
zyrouge <zyrouge@hotmail.com>
2023-10-28 20:55:09 +0530

    refactor: fetch nightly versions

--- app/src/main/java/io/github/zyrouge/symphony/services/AppMeta.kt
+++ app/src/main/java/io/github/zyrouge/symphony/services/AppMeta.kt
@@ -5,6 +5,7 @@ import io.github.zyrouge.symphony.utils.HttpClient
 import io.github.zyrouge.symphony.utils.Logger
 import okhttp3.CacheControl
 import okhttp3.Request
+import org.json.JSONArray
 import org.json.JSONObject
 
 object AppMeta {
@@ -30,10 +31,14 @@ object AppMeta {
     const val izzyOnDroidUrl = "https://apt.izzysoft.de/fdroid/index/apk/$packageName"
     const val fdroidUrl = "https://f-droid.org/en/packages/$packageName"
 
-    /**
-     * Format: v\[yyyy].\[mm].\[versionCode]
-     */
-    fun fetchLatestVersion(): String? {
+    fun isNightlyBuild() = Regex("""-\w{7}$""").containsMatchIn(version)
+
+    fun fetchLatestVersion() = when {
+        isNightlyBuild() -> fetchLatestNightlyVersion()
+        else -> fetchLatestStableVersion()
+    }
+
+    fun fetchLatestStableVersion(): String? {
         try {
             val latestReleaseUrl =
                 "https://api.github.com/repos/$githubRepositoryOwner/$githubRepositoryName/releases/latest"
@@ -51,6 +56,32 @@ object AppMeta {
             }
         } catch (err: Exception) {
             Logger.warn("AppMeta", "version check failed: $err")
+        }
+        return latestVersion
+    }
+
+    fun fetchLatestNightlyVersion(): String? {
+        try {
+            val latestReleaseUrl =
+                "https://api.github.com/repos/$githubRepositoryOwner/$githubRepositoryName/releases"
+            val req = Request.Builder()
+                .url(latestReleaseUrl)
+                .cacheControl(CacheControl.FORCE_NETWORK)
+                .build()
+            val res = HttpClient.newCall(req).execute()
+            val content = res.body?.string() ?: ""
+            val json = JSONArray(content)
+            for (i in 0 until json.length()) {
+                val x = json.getJSONObject(i)
+                val tagName = x.getString("tag_name")
+                val prerelease = x.getBoolean("prerelease")
+                if (prerelease) {
+                    latestVersion = tagName
+                    break
+                }
+            }
+        } catch (err: Exception) {
+            Logger.warn("AppMeta", "nightly version check failed: $err")
         }
         return latestVersion
     }
