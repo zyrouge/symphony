eef21e4fcff5565d80d8c121541fe50407123765
Zyrouge <hello@zyrouge.me>
2024-11-03 21:12:05 +0530

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.target.sdk.get().toInt()
 
-        versionCode = 112
-        versionName = "2024.11.112"
+        versionCode = 113
+        versionName = "2024.11.113"
         versionName = System.getenv("APP_VERSION_NAME") ?: versionName
 
         vectorDrawables {
