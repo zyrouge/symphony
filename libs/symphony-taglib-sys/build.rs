ad84740fe234d381385c83136284641f8668df60
Zyrouge <hello@zyrouge.me>
2026-09-21 00:12:36 +0530

    refactor(taglib-sys): add cmake build & bindgen

--- libs/symphony-taglib-sys/build.rs
+++ libs/symphony-taglib-sys/build.rs
@@ -2,18 +2,27 @@ use std::{env, path::PathBuf};
 
 fn main() {
     let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let taglib_dir = manifest_dir.join("vendor/taglib");
-    println!("cargo:rerun-if-changed={}", taglib_dir.display());
-    let dst = cmake::Config::new(&taglib_dir)
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    println!("cargo:rerun-if-changed=CMakeLists.txt");
+    println!("cargo:rerun-if-changed=vendor/taglib");
+    println!("cargo:rerun-if-changed=vendor/utfcpp");
+    println!("cargo:rerun-if-changed=vendor/zlib");
+    let dst = cmake::Config::new(manifest_dir)
         .define("BUILD_SHARED_LIBS", "OFF")
-        .define("BUILD_TESTING", "OFF")
-        .define("BUILD_EXAMPLES", "OFF")
-        .define("BUILD_BINDINGS", "ON")
-        .define("CMAKE_BUILD_TYPE", "Release")
         .build();
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("lib").display()
     );
     println!("cargo:rustc-link-lib=static=tag");
+    let bindings = bindgen::Builder::default()
+        .header("vendor/taglib/bindings/c/tag_c.h")
+        .allowlist_type("TagLib_.*")
+        .allowlist_function("taglib_.*")
+        .allowlist_var("TAGLIB_.*")
+        .generate()
+        .expect("Unable to generate bindings");
+    bindings
+        .write_to_file(out_dir.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- libs/symphony-taglib-sys/src/lib.rs
+++ libs/symphony-taglib-sys/src/lib.rs
@@ -1,1 +1,3 @@
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+fn test() {}
